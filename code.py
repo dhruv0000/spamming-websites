6524d09c5c781cc9583985ab6f0bcdadac42e3c9
Dhruv Patel <44001539+dhruv0000@users.noreply.github.com>
2019-07-30 15:51:44 +0530

    Updated code.py

--- code.py
+++ code.py
@@ -3,9 +3,9 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome('directory/to/your/chromedriver') 
+driver = webdriver.Chrome('directory/to/your/chromedriver')
 driver.get('https://mycutebaby.in/contest/participant/?n=5cc1a364c4e7c')
-#HTML is a xml language. Basicly I used xpath of the element to find the element(https://selenium-python.readthedocs.io/locating-elements.html)
+#HTML is a xml language.  Basicly I used xpath of the element to find the element(https://selenium-python.readthedocs.io/locating-elements.html)
 button= driver.find_element_by_xpath("//button[@onclick='getLocation()']")
 button.send_keys(Keys.RETURN)
 time.sleep(10) # Let you people actually see something!
